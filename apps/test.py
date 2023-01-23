ce008d3a31953858a99a2612c7def0e75db4e39c
Biniam Garomsa <bag45@duke.edu>
2023-01-22 20:30:15 -0500

    deploy to cloud

--- apps/test.py
+++ apps/test.py
@@ -3,4 +3,15 @@ home_dir = Path(__file__).parents[1]
 img_dir = Path(__file__).parents[0]
 print(home_dir/"data\main_data.csv")
 print(img_dir)
-print(home_dir/r"data\img\topic.png")
+# loc = home_dir/r"data\img\topic.png"
+
+# print(loc, str(loc))
+import os
+import re
+loc = home_dir/r"data\img\topic.png"
+# loc  = str(loc)
+# # "data\img\\topic.png"
+# print(Path(loc))
+print(os.path.abspath(__file__))
+print(Path().parent.absolute())
+print( Path().parent.absolute()/"data/img/topic.png")
